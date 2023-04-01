c9f954b85cd396b6e66b48a9d4e4c3d7d93339a3
dotranminhchu <dotranminhchu@gmail.com>
2023-04-01 20:47:45 +0700

    'push'

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,63 @@
+import { useRef } from "react";
+
+export default function Navbar() {
+
+    return (
+        <>
+            <nav className="bg-gray-800">
+                <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="relative flex items-center justify-between h-16">
+                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+
+                            <button type="button" className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                                <span className="sr-only">Open main menu</span>
+
+                                <svg className="block w-6 h-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                </svg>
+
+                                <svg className="hidden w-6 h-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        </div>
+                        <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
+                            <div className="flex items-center flex-shrink-0">
+                                <img className="block w-auto h-8 lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                                <img className="hidden w-auto h-8 lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                            </div>
+                            <div className="hidden sm:ml-6 sm:block">
+                                <div className="flex space-x-4">
+
+                                    <a href="#" className="px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-md" aria-current="page">Dashboard</a>
+
+                                    <a href="#" className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Team</a>
+
+                                    <a href="#" className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Projects</a>
+
+                                    <a href="#" className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Calendar</a>
+                                </div>
+                            </div>
+                        </div>
+                     
+                    </div>
+                </div>
+
+
+                <div className=" sm:hidden" id="mobile-menu">
+                    <div className="px-2 pt-2 pb-3 space-y-1">
+
+                        <a href="#" className="block px-3 py-2 text-base font-medium text-white bg-gray-900 rounded-md" aria-current="page">Dashboard</a>
+
+                        <a href="#" className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Team</a>
+
+                        <a href="#" className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Projects</a>
+
+                        <a href="#" className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Calendar</a>
+                    </div>
+                </div>
+            </nav>
+
+        </>
+    )
+}
